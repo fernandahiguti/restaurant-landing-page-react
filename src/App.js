6e105dd620ee1bf74f423435e74a2e1712266605
Fernanda Higuti <fernandamh1996@gmail.com>
2023-08-20 14:25:09 -0300

    add About section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Home from "./Components/Home";
+import About from "./Components/About";
+
 
 function App() {
   return (
     <div className="App">
-      <Home>
+      <Home/>
+      <About/>
 
-      </Home>
     </div>
   );
 }
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -54,7 +54,7 @@ const Navbar = () => {
         <a href="">
           <BsCart2 className="navbar-cart-icon" />
         </a>
-        <button className="primary-button">Bookings Now</button>
+        <button className="primary-button">Book Now</button>
       </div>
       <div className="navbar-menu-container">
         <HiOutlineBars3 onClick={() => setOpenMenu(true)} />
