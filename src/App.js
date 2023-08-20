8f64e91e2080fbcd798705f8118c3953f5e53628
Fernanda Higuti <fernandamh1996@gmail.com>
2023-08-20 14:38:15 -0300

    add sections to main page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import './App.css';
 import Home from "./Components/Home";
 import About from "./Components/About";
+import Work from "./Components/Work";
+import Testimonials from "./Components/Testimonials";
+import Contact from './Components/Contact';
+import Footer from './Components/Footer';
+
 
 
 function App() {
@@ -8,6 +13,10 @@ function App() {
     <div className="App">
       <Home/>
       <About/>
+      <Work/>
+      <Testimonials/>
+      <Contact/>
+      <Footer/>
 
     </div>
   );
